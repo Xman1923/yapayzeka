8d325b010ca84d256ef75c84a3bbf265933cf19c
Xman1923 <161951087+Xman1923@users.noreply.github.com>
2024-10-20 16:35:53 +0300

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,5 +1,34 @@
+
 <template>
   <div>
-    <NuxtWelcome />
+    <Header />
+    <div class="container">
+      <NuxtWelcome />
+      <nuxt />
+    </div>
+    <Footer />
   </div>
 </template>
+
+<script>
+import Header from '~/components/common/Header.vue';
+import Footer from '~/components/common/Footer.vue';
+
+export default {
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style>
+/* İsteğe bağlı stiller */
+body {
+  margin: 0;
+  font-family: Arial, sans-serif;
+  background-color: #f4f4f4;
+}
+</style>
+
+
