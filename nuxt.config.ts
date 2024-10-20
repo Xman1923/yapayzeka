be5fe975c40b04e2bbaf48120b80012e6ff644dc
Xman1923 <161951087+Xman1923@users.noreply.github.com>
2024-10-20 16:24:36 +0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,15 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  devtools: { enabled: true },
-  routeRules: {
-    // prerender index route by default
-    '/': { prerender: true },
+export default {
+  head: {
+    title: 'Casino Website',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Online Casino Website' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
-});
+  css: ['~/assets/styles/main.css'],
+  plugins: ['~/plugins/axios.js', '~/plugins/vuetify.js'],
+  buildModules: [],
+  modules: [],
+}
